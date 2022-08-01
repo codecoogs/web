0276dcbdc441d1e3924368f90fc3fcd4e9bfb2f1
LeftAutomated <bryantle2021@gmail.com>
2022-07-31 21:19:00 -0500

    feat(home): create CTA buttons

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,9 +1,41 @@
 import React from "react";
 
-const Home = () => {
+interface HomeButtonProps {
+    link: string;
+    children: string;
+};
+
+const HomeButton = (props: HomeButtonProps) => {
     return (
-        <div className="bg-dark-surface h-screen">
+        <div className="bg-dark-primary w-40 h-10 mx-auto text-center rounded leading-10">
+            <a
+                className="font-semibold"
+                href={props.link}
+            >
+                {props.children}
+            </a>
         </div>
+    );
+};
+
+const Home = () => {
+
+    return (
+        <>
+            <div className="flex flex-wrap">
+                <div className="w-1/5"></div>
+                <div className="w-1/5">
+                    <HomeButton link="/about">About Us</HomeButton>
+                </div>
+                <div className="w-1/5">
+                    <HomeButton link="/membership">Membership</HomeButton>
+                </div>
+                <div className="w-1/5">
+                    <HomeButton link="https://linktr.ee/codecoogs">LinkTree</HomeButton>
+                </div>
+                <div className="w-1/5"></div>
+            </div>
+        </>
     );
 };
 
