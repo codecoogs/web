e92335453ff378cd4ae5c2657f68831d135d86ea
LeftAutomated <bryantle2021@gmail.com>
2022-08-14 20:04:51 -0500

    feat(about): create officer section

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -2,11 +2,14 @@ import React, { useEffect } from "react";
 
 import AboutSideNav from "./AboutSideNav";
 
-const aboutUsDesc = "Founded in September 2021, we are student-led computer science organization at the University of Houston. We focus on socials, teams, competitions, and workshops. We hope to foster an environment to develop exciting projects, refine technical skills, and gain new friends.";
-const socialsDesc = "The socials are a time for members to kick back and relax. Whether it is a sports day at a park or a virtual movie night, our socials are sure to enable our members to get closer to one another to promote the community aspect within our members. Show up to our in-person socials for a good time!";
-const teamsDesc = "Being part of a team in Code Coogs gives members the opportunity to hone their coding skills while making worthwhile friends. Members will work with a diverse group of fellow programmers, and people from all coding and cultural backgrounds. Members will get to experience a collective effort from their own team in producing a hefty end of year project showcased at our banquet";
-const competitionsDesc = "Every two weeks we will hold a competition that will put your skills to the test. Each competition will push you to become a better programmer. The skills you gain from solving these problems will show when you do technical interviews!";
-const workshopsDesc = "Workshops are where our members will develop their programming skills and gain a sense of collaboration with their peers, and gain confidence in their problem-solving skills. Workshops will go over challenging problems that utilize a variety of programming concepts, and students will learn the patterns and techniques used to tackle these problems. It is encouraged to collaborate and bounce ideas off of one another to understand different angles of solving these problems!";
+import { 
+    aboutUsDesc,
+    socialsDesc,
+    teamsDesc,
+    competitionsDesc,
+    workshopsDesc,
+    officers 
+} from "../data/about";
 
 interface AboutSectionProps {
     id: string;
@@ -15,9 +18,31 @@ interface AboutSectionProps {
 
 const AboutSection = (props: AboutSectionProps) => {
     return (
-        <section id={props.id} className="h-full m-8 bg-dark-surface-variant rounded">
+        <section id={props.id} className="h-full m-8 rounded">
             {props.children}
         </section>
+    );
+};
+
+interface OfficerCardProps {
+    key: number
+    avatarURL: string;
+    name: string;
+    position: string;
+}
+
+const OfficerCard = (props: OfficerCardProps) => {
+    return (
+        <div className="bg-dark-surface-variant rounded text-center p-8">
+            <img 
+                className="mx-auto rounded-full"
+                src={props.avatarURL}
+                width="100" 
+                height="100" 
+            />
+            <span className="block font-bold pt-8">{props.name}</span>
+            <span className="block text-sm">{props.position}</span>
+        </div>
     );
 };
 
@@ -54,9 +79,19 @@ const About = () => {
             </AboutSection>
 
             <AboutSection id="officers">
-                <div className="p-8">
-                    <h1 className="text-xl">Officers</h1>
-                    <p>TBA</p>
+                <div className="p-8 text-center">
+                    <h1 className="text-xl font-bold">Officers</h1>
+                    <div className="grid grid-cols-4 gap-4 p-8">
+                        { officers.map((officer, index) => {
+                                return <OfficerCard 
+                                    key={index} 
+                                    avatarURL={officer.avatarURL}
+                                    name={officer.name} 
+                                    position={officer.position} 
+                                />;
+                          })
+                        }
+                    </div>
                 </div>
             </AboutSection>
             <AboutSection id="sponsors">
---
+++ src/data/about.ts
@@ -0,0 +1,78 @@
+export const aboutUsDesc = "Founded in September 2021, we are student-led computer science organization at the University of Houston. We focus on socials, teams, competitions, and workshops. We hope to foster an environment to develop exciting projects, refine technical skills, and gain new friends.";
+export const socialsDesc = "The socials are a time for members to kick back and relax. Whether it is a sports day at a park or a virtual movie night, our socials are sure to enable our members to get closer to one another to promote the community aspect within our members. Show up to our in-person socials for a good time!";
+export const teamsDesc = "Being part of a team in Code Coogs gives members the opportunity to hone their coding skills while making worthwhile friends. Members will work with a diverse group of fellow programmers, and people from all coding and cultural backgrounds. Members will get to experience a collective effort from their own team in producing a hefty end of year project showcased at our banquet";
+export const competitionsDesc = "Every two weeks we will hold a competition that will put your skills to the test. Each competition will push you to become a better programmer. The skills you gain from solving these problems will show when you do technical interviews!";
+export const workshopsDesc = "Workshops are where our members will develop their programming skills and gain a sense of collaboration with their peers, and gain confidence in their problem-solving skills. Workshops will go over challenging problems that utilize a variety of programming concepts, and students will learn the patterns and techniques used to tackle these problems. It is encouraged to collaborate and bounce ideas off of one another to understand different angles of solving these problems!";
+
+export const officers = [
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/C5603AQE7sT0nr27Hhg/profile-displayphoto-shrink_400_400/0/1651125058280?e=1666224000&v=beta&t=0C6tndX7OsR-PMXmW2qOchNCHzfjhb1wcCPwLKpzfMM",
+        name: "Jake Burger",
+        position: "President"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/D4E03AQHUJ67FJ-oGFw/profile-displayphoto-shrink_400_400/0/1645419091689?e=1666224000&v=beta&t=KRci_2gQ73k3baI_u5AkFNQecTfEDz5DriLjl-r0g0E",
+        name: "Adam Nguyen",
+        position: "VP of Internal Affairs"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/C5603AQG3rpUlWkocKg/profile-displayphoto-shrink_400_400/0/1650396692079?e=1666224000&v=beta&t=bBAueSHbX-qBQ0IlmAzqUuZYL3cup4q-0gFPv4dkWL8",
+        name: "Sami Alsayed",
+        position: "VP of External Affairs"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/C4D0BAQHx8BvztOOKLw/company-logo_200_200/0/1635064060156?e=1668643200&v=beta&t=nZoEWW-tLjDXO34agJPfNuDd6YqhLpXynp6CgFTjQsE",
+        name: "Reginald Green",
+        position: "Secretary"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/C5603AQGIIhaUHFVBlA/profile-displayphoto-shrink_400_400/0/1639973530628?e=1666224000&v=beta&t=t0dW8EWWC2UmwtY5d_5Zwu6T68D0YHgzQPZ0sEo-otY",
+        name: "Natalie Severino",
+        position: "Treasurer"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/C4D0BAQHx8BvztOOKLw/company-logo_200_200/0/1635064060156?e=1668643200&v=beta&t=nZoEWW-tLjDXO34agJPfNuDd6YqhLpXynp6CgFTjQsE",
+        name: "Peter Dinh",
+        position: "Marketing Director"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/C4D0BAQHx8BvztOOKLw/company-logo_200_200/0/1635064060156?e=1668643200&v=beta&t=nZoEWW-tLjDXO34agJPfNuDd6YqhLpXynp6CgFTjQsE",
+        name: "Wyatt Fernandez",
+        position: "Activities Director"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/C4D03AQGmR9l-dyu_8w/profile-displayphoto-shrink_400_400/0/1646352753099?e=1666224000&v=beta&t=BudjmvG6ISI5XrO1HWO-Z_br9OUuN9l0Jh7zk-TTU04",
+        name: "Jennifer Hernandez",
+        position: "Artist"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/C5603AQEdkJLEiO73AA/profile-displayphoto-shrink_400_400/0/1648152500026?e=1666224000&v=beta&t=zpOuREjj6qZpAv05AH5uyQudJQ1w0Kbz0h1ExalkVfY",
+        name: "Benjamin Guzman",
+        position: "Competitions Director"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/D4D03AQHJWNuXJycc4w/profile-displayphoto-shrink_400_400/0/1645570570954?e=1666224000&v=beta&t=8mSKvl3c6U1NmlTftpJ13cJNPkkVo9XkzLVWqIVKxlQ",
+        name: "Fernando Ramirez",
+        position: "Teams Director"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/C4D0BAQHx8BvztOOKLw/company-logo_200_200/0/1635064060156?e=1668643200&v=beta&t=nZoEWW-tLjDXO34agJPfNuDd6YqhLpXynp6CgFTjQsE",
+        name: "Omar Meziou",
+        position: "Workshops Director"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/C4D0BAQHx8BvztOOKLw/company-logo_200_200/0/1635064060156?e=1668643200&v=beta&t=nZoEWW-tLjDXO34agJPfNuDd6YqhLpXynp6CgFTjQsE",
+        name: "Hanya Elmasik",
+        position: "Historian"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/C4D0BAQHx8BvztOOKLw/company-logo_200_200/0/1635064060156?e=1668643200&v=beta&t=nZoEWW-tLjDXO34agJPfNuDd6YqhLpXynp6CgFTjQsE",
+        name: "Alexandria Harp",
+        position: "Historian"
+    },
+    {
+        avatarURL: "https://media-exp1.licdn.com/dms/image/C5603AQGMFDlpvcjM2g/profile-displayphoto-shrink_400_400/0/1643153551954?e=1666224000&v=beta&t=-COeInxha0gjb6DHFdUwixKZ2dA2Fiad1oT6vcKdGqs",
+        name: "Bryant Le",
+        position: "Wizard"
+    }
+];
