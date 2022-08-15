53d9ab68ebcb5d4a7f3f16f5ee9af6abb76f4eda
LeftAutomated <bryantle2021@gmail.com>
2022-08-14 20:06:12 -0500

    feat(members): create members page component

---
+++ src/members/Members.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+
+const Members = () => {
+    useEffect(() => {
+        document.title = "Members";
+    });
+
+    return (
+        <div className="m-8 text-white bg-dark-surface-variant rounded h-screen">
+            <h1 className="p-8">Membership</h1>
+            <ul className="px-8">
+                <li>
+                    <span>T-shirt</span>
+                </li>
+                <li>
+                    <span>Discord Role</span>
+                </li>
+                <li>
+                    <span>Teams System</span>
+                </li>
+                <li>
+                    <span>Swag</span>
+                </li>
+                <li>
+                    <span>Socials</span>
+                </li>
+                <li>
+                    <span>Special workshops</span>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Members;
