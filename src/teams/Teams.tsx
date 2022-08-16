47fdcd8c74c2d953a85c05098252559ff6689789
LeftAutomated <bryantle2021@gmail.com>
2022-08-15 23:14:59 -0500

    fix(teams): fix team cards layout

--- src/teams/Teams.tsx
+++ src/teams/Teams.tsx
@@ -6,7 +6,7 @@ interface TeamCardProps {
 
 const TeamCard = (props: TeamCardProps) => {
     return (
-        <li className="bg-dark-surface-variant rounded h-full text-white p-8 m-8">
+        <li className="bg-dark-surface-variant rounded text-white p-8 m-8">
             {props.children}
         </li>
     );
@@ -15,8 +15,8 @@ const TeamCard = (props: TeamCardProps) => {
 const Teams = () => {
     return (
         <div className="bg-dark-surface">
-            <div className="table my-0 mx-auto">
-                <ul className="flex flex-col md:flex-row m-8 bg-dark-surface pb-4">
+            <div className="text-center">
+                <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 md:flex-row m-8 bg-dark-surface pb-4">
                     <TeamCard>
                         <span>Git Gud</span>
                     </TeamCard>
