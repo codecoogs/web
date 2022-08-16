e5804776c14595d70f593a281806dc2fdbe1409c
LeftAutomated <bryantle2021@gmail.com>
2022-08-15 23:27:31 -0500

    feat(layout): add route to events page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Layout from "./common/Layout";
 
 import Home from "./home/Home";
 import About from "./about/About";
+import Events from "./events/Events";
 import Teams from "./teams/Teams";
 import Projects from "./projects/Projects";
 import Members from "./members/Members";
@@ -20,6 +21,7 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
+                    <Route path="/events" element={<Events />} />
                     <Route path="/teams" element={<Teams />} />
                     <Route path="/projects" element={<Projects />} />
                     <Route path="/members" element={<Members />} />
