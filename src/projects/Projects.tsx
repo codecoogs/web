43dbb3a15639ec2226d88a599f6557c8ced272f8
LeftAutomated <bryantle2021@gmail.com>
2022-08-24 07:37:48 -0500

    feat(projects): add project links

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 
+import { projects } from "../data/projects";
+
+interface LinksObj {
+    source: string;
+    demo: string;
+}
+
 interface ProjectCardProps {
-    children: React.ReactNode;
+    key: number;
+    name: string;
+    creator: string;
+    links: LinksObj;
 };
 
 const ProjectCard = (props: ProjectCardProps) => {
+    const {
+        source,
+        demo
+    } = props.links;
+
     return (
         <li className="flex flex-col bg-dark-surface-variant rounded text-white p-8 m-8">
-            {props.children}
+            <span>{props.name}</span>
+            <span className="pt-8">{props.creator}</span>
+            {source !== "" && <a className="text-dark-primary underline pt-8" href={source}>Source</a>}
+            {demo !== "" && <a className="text-dark-primary underline" href={demo}>Demo</a>}
         </li>
     );
 };
@@ -17,26 +35,15 @@ const Projects = () => {
         <div className="bg-dark-surface">
             <div className="text-center">
                 <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m-8 bg-dark-surface pb-4">
-                    <ProjectCard>
-                        <span>Robotic Arm</span>
-                        <span className="pt-8">Git Gud</span>
-                    </ProjectCard>
-                    <ProjectCard>
-                        <span>2D Platformer</span>
-                        <span className="pt-8">Team O(n)</span>
-                    </ProjectCard>
-                    <ProjectCard>
-                        <span>Number Handwriting ML Program</span>
-                        <span className="pt-8">Team O(n)</span>
-                    </ProjectCard>
-                    <ProjectCard>
-                        <span>Spotify Bot</span>
-                        <span className="pt-8">Dream Team</span>
-                    </ProjectCard>
-                    <ProjectCard>
-                        <span>Twitter Bot</span>
-                        <span className="pt-8">Dream Team</span>
-                    </ProjectCard>
+                    { projects.map((project, index) => {
+                            return <ProjectCard
+                                    key={index}
+                                    name={project.name}
+                                    creator={project.creator}
+                                    links={project.links}
+                                   />
+                      })
+                    }
                 </ul>
             </div>
         </div>
