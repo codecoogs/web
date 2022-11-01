606166feec79bf30316c14703a97573543a208a5
programwithjohn <john.cox0321@gmail.com>
2022-11-01 17:26:04 -0500

    added enlarge card on hover

--- src/teams/Teams.tsx
+++ src/teams/Teams.tsx
@@ -9,7 +9,7 @@ interface TeamCardProps {
 
 const TeamCard = (props: TeamCardProps) => {
     return (
-        <li className="bg-dark-surface-variant rounded-lg text-white p-8 m-8">
+        <li className="bg-dark-surface-variant rounded-lg text-white p-8 m-8 md:hover:scale-11">
             {props.children}
         </li>
     );
@@ -32,11 +32,11 @@ const Teams = () => {
                             return(
                                 <div>
                                     <h2 className="p-8 font-bold text-4xl text-center underline underline-offset-8">{year.year}</h2>  
-                                    <ul className="grid grid-cols-3 gap-4">
+                                    <ul className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                                         {
                                             year.info.map((team, index ) => {
                                                 return (
-                                                    <TeamCard key={index} >  
+                                                    <TeamCard key={index}>  
                                                         <img src={team.photo} alt={`${team.name} photo`}/>
                                                         <h1>{team.name}</h1>
                                                     </TeamCard>
