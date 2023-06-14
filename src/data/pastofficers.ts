75fe23675a0cbc14998cc591bf426eff2d8a63cf
Jake Burger <jaykegamer12@gmail.com>
2023-06-13 23:15:24 -0500

    feat(data): created past officers comp

---
+++ src/data/pastofficers.ts
@@ -0,0 +1,7 @@
+export const pastofficersinfo = [
+    {
+        name: "Tailer Nguyen",
+        position: "President 2021 - 2022",
+        linkedin: "https://www.linkedin.com/in/tailer-nguyen/"
+    }
+]
