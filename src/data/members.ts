395add1dfe441cbf776f1552ef0c6bbb4efd13a5
Bryant Le <bryantle2021@gmail.com>
2023-09-18 23:45:24 -0500

    feat(members): update member benefits

--- src/data/members.ts
+++ src/data/members.ts
@@ -1,10 +1,10 @@
 export const benefits = [
-    "Placement on one of our five teams",
+    "Placement on one of our project teams",
     "Resume sent to our sponsors",
     "Eligibility for our ICPC teams",
     "Access to team project supplies worth up to $250",
     "Chance for up to $500 in scholarships and prizes",
-    "Access to workshops folder and extra problems",
+    "Access to workshop materials and supporting learning activities",
     "Member exclusive events and activities",
     "Free food at our meetings and events"
 ];
