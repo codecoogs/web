31750c6afa883bd764ba18f800e1d6303f4765b6
Bryant Le <bryantle2021@gmail.com>
2023-11-25 15:59:17 -0600

    feat(about): add 2022 fall officers

--- src/data/officers/index.ts
+++ src/data/officers/index.ts
@@ -1,10 +1,12 @@
 import Fall2021 from "./2021Fall";
 import Spring2022 from "./2022Spring";
+import Fall2022 from "./2022Fall";
 import Fall2023 from "./2023Fall";
 
 const officers = [
   Fall2021,
   Spring2022,
+  Fall2022,
   Fall2023
 ];
 
