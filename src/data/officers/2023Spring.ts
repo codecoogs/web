c39873e4ef72b74cd9ae4dcd536e7be09462fdef
Bryant Le <bryantle2021@gmail.com>
2023-11-25 22:15:56 -0600

    feat(about): add 2023 spring officers

---
+++ src/data/officers/2023Spring.ts
@@ -0,0 +1,122 @@
+import yuryPhoto from "./../../assets/officers/2023spring/yury.png";
+import jakePhoto from "./../../assets/officers/2023spring/jake.png";
+import samiPhoto from "./../../assets/officers/2023spring/sami.png";
+import adamPhoto from "./../../assets/officers/2023spring/adam.png";
+import reginaldPhoto from "./../../assets/officers/2023spring/reginald.png";
+import hieuPhoto from "./../../assets/officers/2023spring/hieu.jpg";
+import fernandoPhoto from "./../../assets/officers/2023spring/fernando.png";
+import hanyaPhoto from "./../../assets/officers/2023spring/hanya.png";
+import benjaminPhoto from "./../../assets/officers/2023spring/benjamin.png";
+import omarPhoto from "./../../assets/officers/2023spring/omar.png";
+import albamariaPhoto from "./../../assets/officers/2023spring/albamaria.jpg";
+import bryantPhoto from "./../../assets/officers/2023spring/bryant.png";
+import alexandriaPhoto from "./../../assets/officers/2023spring/alexandria.png";
+
+const officers2023Spring = [
+  {
+    name: "Yury Ionov",
+    position: "President",
+    photo: yuryPhoto,
+    socials: {
+      linkedin: "https://www.linkedin.com/in/yuryion/",
+    }
+  },
+  {
+    name: "Jake Burger",
+    position: "Senior Advisor",
+    photo: jakePhoto,
+    socials: {
+      linkedin: "https://www.linkedin.com/in/jake-d-burger/",
+    }
+  },
+  {
+    name: "Sami Alsayed",
+    position: "Vice President External",
+    photo: samiPhoto,
+    socials: {
+      linkedin: "https://www.linkedin.com/in/alsayedsami/",
+    }
+  },
+  {
+    name: "Adam Nguyen",
+    position: "Vice President Internal",
+    photo: adamPhoto,
+    socials: {
+      linkedin: "https://linkedin.com/in/ah-nguyen/",
+    }
+  },
+  {
+    name: "Reginald Green",
+    position: "Secretary",
+    photo: reginaldPhoto,
+    socials: {
+      linkedin: "https://www.linkedin.com/in/reginaldgreen1102/",
+    }
+  },
+  {
+    name: "Hieu Bui",
+    position: "Activities Director",
+    photo: hieuPhoto,
+    socials: {
+      linkedin: "https://www.linkedin.com/in/hbui0107/",
+    }
+  },
+  {
+    name: "Fernando Ramirez",
+    position: "Teams Director",
+    photo: fernandoPhoto,
+    socials: {
+      linkedin: "https://www.linkedin.com/in/fndo/",
+    }
+  },
+  {
+    name: "Hanya Elmasik",
+    position: "Marketing Director",
+    photo: hanyaPhoto,
+    socials: {
+      linkedin: "https://www.linkedin.com/in/hanya-elmasik-a78b95232/",
+    }
+  },
+  {
+    name: "Benjamin Guzman",
+    position: "Competitions Director",
+    photo: benjaminPhoto,
+    socials: {
+      linkedin: "https://www.linkedin.com/in/benjamin-guzman-4b6732235/",
+    }
+  },
+  {
+    name: "Omar Meziou",
+    position: "Workshops Director",
+    photo: omarPhoto,
+    socials: {
+      linkedin: "https://www.linkedin.com/in/omar-meziou/",
+    }
+  },
+  {
+    name: "Albamaria Rodrigues",
+    position: "Artist",
+    photo: albamariaPhoto,
+    socials: {
+      linkedin: "https://www.linkedin.com/in/albamaria-rodrigues-38b129216/",
+    }
+  },
+  {
+    name: "Bryant Le",
+    position: "Wizard",
+    photo: bryantPhoto,
+    socials: {
+      linkedin: "https://www.linkedin.com/in/bnle/",
+    }
+  },
+  {
+    name: "Alexandria Harp",
+    position: "Historian",
+    photo: alexandriaPhoto,
+    socials: {
+      linkedin: "https://www.linkedin.com/in/alharp3/",
+    }
+  }
+];
+
+export default officers2023Spring;
--- src/data/officers/index.ts
+++ src/data/officers/index.ts
@@ -1,12 +1,14 @@
 import Fall2021 from "./2021Fall";
 import Spring2022 from "./2022Spring";
 import Fall2022 from "./2022Fall";
+import Spring2023 from "./2023Spring";
 import Fall2023 from "./2023Fall";
 
 const officers = [
   Fall2021,
   Spring2022,
   Fall2022,
+  Spring2023,
   Fall2023
 ];
 
