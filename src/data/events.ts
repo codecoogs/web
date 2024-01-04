47350fc77b5292b76fd5ebe210431e880c529738
Jake Burger <jaykegamer12@gmail.com>
2024-01-04 03:45:47 -0600

    fix(events): fix calendar design

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,1 +1,1 @@
-export const eventGoogleCalendarLink = "https://calendar.google.com/calendar/embed?mode=AGENDA&showPrint=0&showNav=0&src=ah0fj55libf6m1s1ofm2ah8foo%40group.calendar.google.com&ctz=America%2FChicago";
+export const eventGoogleCalendarLink = "https://embed.styledcalendar.com/#q7kCoBaJxjih9SnUR4um";
