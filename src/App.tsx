c2f164f2120b333df5a900c5af86aa2cbd5b9708
weabruhnie <howtodrawapicture@gmail.com>
2024-06-29 00:24:13 +0700

    adding pdf

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Layout from "./common/Layout";
 import Home from "./home/Home";
 import Events from "./events/Events";
 import Members from "./members/Members";
+import Sponsors from "./sponsors/Sponsors";
 
 const App = () => {
     return (
@@ -19,6 +20,7 @@ const App = () => {
                     <Route path="/" element={<Home />} />
                     <Route path="/events" element={<Events />} />
                     <Route path="/join" element={<Members />} />
+                    <Route path="/sponsors" element={<Sponsors />} />
                 </Routes>
             </Layout>
         </BrowserRouter>
---
+++ src/sponsors/Sponsors.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import HTMLFlipBook from "react-pageflip";
+import { Document, Page as ReactPDFPage, pdfjs } from "react-pdf";
+
+import sponsorship from "../../public/sponsorship.pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+const width = 720;
+const height = 1280;
+
+const Page = (props: { pageNumber: number }) => {
+  return (
+    <div>
+      <ReactPDFPage pageNumber={props.pageNumber} width={width} />
+    </div>
+  );
+};
+
+const Sponsors = () => {
+  useEffect(() => {
+    document.title = "Sponsors";
+  }, []);
+
+  console.log("TEST")
+
+  return (
+    <div>
+        <h1>TEST</h1>
+      <Document file={sponsorship}>
+        <HTMLFlipBook
+          style={{}}
+          // children={{}}
+          startPage={0}
+          width={width}
+          height={height}
+          drawShadow={true}
+          flippingTime={10}
+          usePortrait={false}
+          startZIndex={0}
+          autoSize={false}
+          clickEventForward={false}
+          useMouseEvents={false}
+          swipeDistance={0}
+          showPageCorners={false}
+          disableFlipByClick={false}
+          size="stretch"
+          minWidth={315}
+          maxWidth={width}
+          minHeight={400}
+          maxHeight={height}
+          maxShadowOpacity={0.5}
+          showCover={true}
+          mobileScrollSupport={true}
+          onFlip={() => {}}
+          onChangeOrientation={() => {}}
+          onChangeState={() => {}}
+          className="demo-book"
+        >
+          <Page pageNumber={1} />
+        </HTMLFlipBook>
+      </Document>
+    </div>
+  );
+};
+
+export default Sponsors;
