14fc759226b26e7cc1cc9b5cd245fdf498614287
weabruhnie <52168604+weabruhnie@users.noreply.github.com>
2024-10-23 14:45:20 -0500

    Added teams photo into database

---
+++ src/data/teams/2024Year.ts
@@ -0,0 +1,47 @@
+// TODO: Add Team 7 Photo
+
+const year2024Teams = [
+  {
+    name: "Circuit Cider",
+    photo: "/assets/teams/2024Year/1.webp",
+    leads: [],
+    github: "https://github.com/CodeCoogs-Team1-GameDev/CoCo_MultiplayerGame",
+  },
+  {
+    name: "Team 2",
+    photo: "/assets/teams/2024Year/2.webp",
+    leads: [],
+    github: "https://github.com/sebgutierrez/Scorched-Earth",
+  },
+  {
+    name: "Team 3",
+    photo: "/assets/teams/2024Year/3.webp",
+    leads: [],
+    github: "https://github.com/Polyguins",
+  },
+  {
+    name: "Team 4",
+    photo: "/assets/teams/2024Year/4.webp",
+    leads: [],
+    github: "https://github.com/AlfredoRuiz-CS/Social-Media-Dashboard",
+  },
+  {
+    name: "Team 5",
+    photo: "/assets/teams/2024Year/5.webp",
+    leads: [],
+    github: "https://github.com/rydb/CircuitCider",
+  },
+  {
+    name: "Team 6",
+    photo: "/assets/teams/2024Year/6.webp",
+    leads: [],
+    github: "https://github.com/Interview-GPT",
+  },
+  {
+    name: "Team 7",
+    leads: [],
+    github: "https://github.com/jzurc7/wealth-wise",
+  },
+];
+
+export default year2024Teams;
